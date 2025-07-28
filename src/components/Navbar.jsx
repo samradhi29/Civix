d4d6b3eb62a4884863416ab1351b1dd5c9682842
Verma-MK <pncmfp@gmail.com>
2025-07-28 19:09:49 +0530

    Logo redesign

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -161,21 +161,7 @@ const Navbar = () => {
         <button onClick={() => { setMobileMenuOpen(false); navigate('/'); }} className="flex items-center gap-2 hover:text-emerald-500 transition-colors duration-300" style={{width: "74px", marginLeft: "21px"}}
     
 >
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="h-6 w-6 text-emerald-500"
-          >
-            <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
-            <circle cx="12" cy="10" r="3" />
-          </svg> */}
+          
           <img src={logo} alt="" />
           <span id="logo" className="text-xl font-bold" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>  </span>
         </button>
