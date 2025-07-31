93adc8cd76a3bdc12d51bbf4f37727349a1a9a5a
Tanmay Kalra <tanmaykalra0910@gmail.com>
2025-07-31 18:46:11 +0530

    UI Enhancement of About Page

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-mimport { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { toast, ToastContainer } from 'react-toastify';
 
 const AdminDashboard = () => {
